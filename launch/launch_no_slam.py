ca567856d6dbdb42624da449666bfd957724fce3
Myles Pasetsky <myles.pasetsky@gmail.com>
2024-09-13 14:31:37 +0000

    Separate launch file with slam and without

--- launch/launch_no_slam.py
+++ launch/launch_no_slam.py
@@ -1,7 +1,4 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
-from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
@@ -9,35 +6,6 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
-
-    ### RTABMAP configuration ###
-
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    qos = LaunchConfiguration('qos')
-    localization = LaunchConfiguration('localization')
-    
-    parameters={
-          'frame_id':'base_footprint',
-          'use_sim_time':use_sim_time,
-          'subscribe_depth':False,
-          'subscribe_rgb':False,
-          'subscribe_scan':True, # only use lidar for now
-          'approx_sync':True,
-          'use_action_for_goal':True,
-          'qos_scan':qos,
-          'qos_imu':qos,
-          'Reg/Strategy':'1',
-          'Reg/Force3DoF':'true',
-          'RGBD/NeighborLinkRefining':'True',
-          'Grid/RangeMin':'0.2', # ignore laser scan points on the robot itself
-          'Optimizer/GravitySigma':'0' # Disable imu constraints (we are already in 2D)
-    }
-    
-    remappings=[
-          ('scan', '/scan')]
-
-
-    ### AutoDrive simulator setup ### 
     config_file_path = os.path.join(
         get_package_share_directory('autodrive_iros_2024'),  # Replace with your package name
         'config', 
@@ -53,23 +21,6 @@ def generate_launch_description():
     )
     
     return LaunchDescription([
-
-        ### ARGUMENTS ###
-
-        DeclareLaunchArgument(
-            'use_sim_time', default_value='true',
-            description='Use simulation (Gazebo) clock if true'),
-        
-        DeclareLaunchArgument(
-            'qos', default_value='2',
-            description='QoS used for input sensor topics'),
-            
-        DeclareLaunchArgument(
-            'localization', default_value='false',
-            description='Launch in localization mode.'),
-
-        ### TRANSFORMS ###
-
         # Static Transform: world -> map
         Node(
             package='tf2_ros',
@@ -134,27 +85,6 @@ def generate_launch_description():
             parameters=[],
             output='screen'
         ),
-
-
-        # rtabmap nodes
-
-        # SLAM mode:
-        Node(
-            condition=UnlessCondition(localization),
-            package='rtabmap_slam', executable='rtabmap', output='screen',
-            parameters=[parameters],
-            remappings=remappings,
-            arguments=['-d']), # This will delete the previous database (~/.ros/rtabmap.db)
-            
-        # Localization mode:
-        Node(
-            condition=IfCondition(localization),
-            package='rtabmap_slam', executable='rtabmap', output='screen',
-            parameters=[parameters,
-              {'Mem/IncrementalMemory':'False',
-               'Mem/InitWMWithAllNodes':'True'}],
-            remappings=remappings),
-
 
         # Include the simulator bringup launch file
         IncludeLaunchDescription(
